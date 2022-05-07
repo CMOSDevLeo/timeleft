34889d320a2c30d1fe9a18afb581c8b49f0d4bd6
CMOSDevLeo <105015459+CMOSDevLeo@users.noreply.github.com>
2022-05-07 12:07:42 +0200

    seconds calculation + format
    seconds are now calculated properly, before it calculated only percent of minute

--- timeleft.py
+++ timeleft.py
@@ -1,28 +1,28 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3 
 # program gives you the time which it takes to view the remaining video in some multiplied time
 # created symbolic for not having to type .py after timeleft
 
 import sys
-multi = float(sys.argv[1])
-time = float(sys.argv[2])
+multi = int(sys.argv[1])
+minutes = int(sys.argv[2])
 result = 0
 
-#print("multi:", multi)
-#print("timeleft:", time)
+seconds = minutes * 60
 
 if multi == 25:
-    result = time/1.25
+    result = round(seconds/1.25)
 elif multi == 5 or multi == 50:
-    result = time/1.5
+    result = round(seconds/1.5)
 elif multi == 75:
-    result = time/1.75
+    result = round(seconds/1.75)
 elif multi == 2:
-    result = time/2
+    result = seconds/2
 else:
-    print("correct input: timeleft [multi] [timeleft]")
+    print("correct input: timeleft [multi] [timeleft in minutes]")
 
-diff = time - result
+secDiff = seconds - result
+newMin, newSec= divmod(result, 60)
+minDiff, secDiff = divmod(secDiff, 60)
 
-print ("At 1," + str(int(multi)) + "x remaining time is: " + str(round(result,2)))
-print("\nYou'd safe " + str(round(diff,2)) + " minutes.")
+print ("At 1," + str(multi) + "x remaining time is: " + str(newMin) + "." + str(newSec))
+print("\nYou'd safe " + str(minDiff) + "." + str(secDiff) + " minutes") 
